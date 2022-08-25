4a115438ec1aecaa34a77d50e8c3831dcdd8687d
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-25 16:16:47 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const butStart = document.querySelector('[data-start]');
+const butStop = document.querySelector('[data-stop');
+const body = document.querySelector('body');
+let colorSwitch = null;
+
+butStart.addEventListener('click', () => {
+  colorSwitch = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  butStart.disabled = true;
+});
+
+butStop.addEventListener('click', () => {
+  clearInterval(colorSwitch);
+  butStart.disabled = false;
+});
